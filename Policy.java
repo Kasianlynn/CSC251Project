d3a1089f51623ce5bdad76821ee5fa7e6af48dad
Kasianlynn <159495173+Kasianlynn@users.noreply.github.com>
2024-03-18 10:31:25 -0400

    Update Policy.java
    Added documentation

--- Policy.java
+++ Policy.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Policy
 {
 
+      //fields
       private int policyNumber;
       private String providerName;
       private String firstName;
@@ -12,6 +13,7 @@ public class Policy
       private double height;
       private double weight;
 
+      //Construct new Policy object with default values
       public Policy()
       {
          policyNumber = 0;
@@ -23,94 +25,124 @@ public class Policy
          height = 0;
          weight = 0;
       }
-      
+
+      //@param policyNumber set policy number
        public void setPolicyNumber(int policyNumber)
       {
          this.policyNumber = policyNumber;
       }
-      
+
+      //@param providerName set privder name
       public void setProviderName(String providerName)
       {
          this.providerName = providerName;
       }
-      
+
+
+      //@param firstName set policy holder's first name
       public void setFirstName(String firstName)
       {
          this.firstName = firstName;
       }
-      
+
+      //@param lastName set polic holder's last name
       public void setLastName(String lastName)
       {
          this.lastName = lastName;
       }
-      
+
+      //@param age set policy holder's age
       public void setAge(int age)
       {
          this.age = age;
       }
-      
+
+      //@param smokingStatus set policy holder's smoking status (smoker or non-smoker)
       public void setSmokingStatus(String smokingStatus)
       {
          this.smokingStatus = smokingStatus;
       }
-      
+
+      //@param height set policy holder's height in inches
       public void setHeight(double height)
       {
          this.height = height;
       }
-      
+
+
+      //@param weight set policy holder's weight in pounds
       public void setWeight(double weight)
       {
          this.weight = weight;
       }
       
       
-      
+      //@return the policy number
       public int getPolicyNumber()
       {
          return policyNumber;
       }
-      
+
+      //@return the Provider name
       public String getProviderName()
       {
          return providerName;
       }
-      
+
+      //@return the first name of the policy holder
       public String getFirstName()
       {
          return firstName;
       }
-      
+
+      //@return the last name of the policy holder
       public String getLastName()
       {
          return lastName;
       }
-      
+
+      //@return the age of the policy holder
       public int getAge()
       {
          return age;
       }
-      
+
+
+      //@return the smoking status of the policy holder
       public String getSmokingStatus()
       {
          return smokingStatus;
       }
-      
+
+
+      //@return the height of the policy holder
       public double getHeight()
       {
          return height;
       }
-      
+
+
+      //@return the weight of the policy holder
       public double getWeight()
       {
          return weight;
       }
-      
+
+      /**
+      * Calculates the policy holder's BMI based on the height and weight
+      *@param height The height in inches
+      *@param weight The weight in pounds
+      *@return The calulated BMI
+      */
       public double calculateBMI(double weight, double height)
       {
          return (weight * 703) / (height * height);
       }
-      
+
+      /**
+      * Calulate the insurance price based on baseFee and additional fees
+      *@return The calculated insurance price
+      */
       public double calculateInsurancePrice()
       {
          double baseFee = 600.0;
@@ -135,7 +167,13 @@ public class Policy
          
          return baseFee + additionalFee;
       } 
-      
+
+
+   /**
+   *   Asks user for input and reads input
+   *   plugs input into Policy class
+   *   Displays poolicy details
+   */
    public static void main(String[] args)
    {
       Scanner keyboard = new Scanner(System.in);
